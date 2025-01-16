a012b6fd0947a2927abc461a34a4d9a8c5624c31
kon5271220000 <kon527122000@gmail.com>
2025-01-16 16:43:59 +0700

    finish refactor

--- script.js
+++ script.js
@@ -8,79 +8,95 @@ class Book {
     }
 }
 
-const tableData = document.querySelector("#data_table tbody")
+class Library {
+    constructor(){
+        this.myLibrary = []
+        this.tableData = document.querySelector("#data_table tbody")
+        this.initialize()
+    }
 
-function display_book(book) {
-    const row = document.createElement("tr")
+    initialize(){
+        document.getElementById("book_info").addEventListener("submit", (event) =>{
+            event.preventDefault()
+        
+            const name = document.getElementById("name").value
+            const author = document.getElementById("author").value
+            const pages = document.getElementById("pages").value
+            const read = document.getElementById("read").checked
+        
+            const new_book = new Book(name, author, pages, read)
+        
+            this.addBook(new_book)
+            this.display_book(new_book)
+        
+            document.getElementById("book_info").reset()
+        })
+    }
 
-    const name_cell = document.createElement("td")
-    name_cell.textContent = book.name
-    row.appendChild(name_cell)
+    addBook(book){
+        this.myLibrary.push(book)
+    }
 
-    const author_cell = document.createElement("td")
-    author_cell.textContent = book.author
-    row.appendChild(author_cell)
+    display_book(book){
+        const row = document.createElement("tr")
 
-    const pages_cell = document.createElement("td")
-    pages_cell.textContent = book.pages
-    row.appendChild(pages_cell)
+        const name_cell = document.createElement("td")
+        name_cell.textContent = book.name
+        row.appendChild(name_cell)
 
-    const read_cell = document.createElement("td")
-    read_cell.textContent = book.read ? "Yes": "No"
-    row.appendChild(read_cell)
+        const author_cell = document.createElement("td")
+        author_cell.textContent = book.author
+        row.appendChild(author_cell)
 
-    const change_cell = document.createElement("td")
-    const change_btn = document.createElement("button")
-    change_btn.textContent = "Change (Yes/No)"
-    change_btn.addEventListener("click", function(event) {
-        book.read = !book.read
-        read_cell.textContent = book.read ? "Yes": "No"
-    })
-    change_cell.appendChild(change_btn)
-    row.appendChild(change_cell)
+        const pages_cell = document.createElement("td")
+        pages_cell.textContent = book.pages
+        row.appendChild(pages_cell)
 
+        const read_cell =document.createElement("td")
+        read_cell.textContent = book.read ? "Yes":"No"
+        row.appendChild(read_cell)
 
-    const delete_cell = document.createElement("td")
-    const delete_btn = document.createElement("button")
-    delete_btn.textContent = "Remove"
-    delete_btn.addEventListener("click", function(event) {
-        row.remove()
-        const index = myLibrary.indexOf(book)
-        if(index > -1){
-            myLibrary.slice(index, 1)
-        }
-    })
-    delete_cell.appendChild(delete_btn)
-    row.appendChild(delete_cell)
+        const change_cell = document.createElement("td")
+        const change_btn = document.createElement("button")
+        change_btn.textContent = "Change (Yes/No)"
+        change_btn.addEventListener('click', () => {
+            book.read = !book.read
+            read_cell.textContent = book.read ? "Yes":"No"
+        })
+        change_cell.appendChild(change_btn)
+        row.appendChild(change_cell)
 
-    tableData.appendChild(row) 
+        const delete_cell = document.createElement("td")
+        const delete_btn = document.createElement("button")
+        delete_btn.textContent = "Remove"
+        delete_btn.addEventListener('click', () => {
+            row.remove()
+            const index = this.myLibrary.indexOf(book)
+            if(index > -1){
+                this.myLibrary.slice(index, 1)
+            }
+        })
+        delete_cell.appendChild(delete_btn)
+        row.appendChild(delete_btn)
+
+        this.tableData.appendChild(row)
+    }
+
+    loadInitialBook(books){
+        books.forEach(book => {
+            this.addBook(book)
+            this.display_book(book)
+        })
+    }
+   
 }
 
-const book_1 = new Book("Thus spoke Zarathustra", "Friendrich Nietzsche", 352, true)
-const book_2 = new Book("Capitalist realism: IS there no Alternative", "Mark Fisher", 81, false)
-const book_3 = new Book("Discipline and Punish: The birth of the prison", "Micheal Foucault", 333, true)
+const myLibra = new Library();
 
+const initialBooks = [
+    new Book("Thus spoke Zarathustra", "Friendrich Nietzsche", 352, true),
+    new Book("Capitalist realism: Is there no Alternative?", "Mark Fisher", 81, true),
+    new Book("Discipline and Punish: The birth of the prison", "Micheal Foucault", 333, true)
+];
 
-myLibrary.push(book_1, book_2, book_3)
-
-for(let i = 0; i < myLibrary.length; i++) {
-    display_book(myLibrary[i])
-}
-
-document.getElementById("book_info").addEventListener("submit", function(event){
-    event.preventDefault()
-
-    const name = document.getElementById("name").value
-    const author = document.getElementById("author").value
-    const pages = document.getElementById("pages").value
-    const read = document.getElementById("read").checked
-
-    const new_book = new Book(name, author, pages, read)
-
-    console.log(read)
-
-    myLibrary.push(new_book)
-    display_book(new_book)
-
-    document.getElementById("book_info").reset()
-})
+myLibra.loadInitialBook(initialBooks);
